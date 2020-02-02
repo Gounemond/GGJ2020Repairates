0560d2f7f015fce44a3f07c5a6bb48d11ab968a1
leonardobarilani <il_leonardo@hotmail.it>
2020-02-02 14:28:04 +0100

    created score screen

---
+++ GGJ2020_ARRH/Assets/Scripts/GameManager/ScoreSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class ScoreSingleton
+{
+    public int stage;
+    ScoreSingleton()
+    {
+    }
+    private static readonly object padlock = new object();
+    private static ScoreSingleton instance = null;
+    public static ScoreSingleton Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new ScoreSingleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ GGJ2020_ARRH/Assets/UpdateScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UpdateScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int stage = ScoreSingleton.Instance.stage;
+        GetComponent<Text>().text = "You arrrh-ivved at stage " + stage + ", matey!";
+        StartCoroutine(Sleep());
+    }
+
+    IEnumerator Sleep()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("0_HomeScreen");
+    }
+}
