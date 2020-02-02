24e3183f5a1490f6939d6108e183aa6f8c51fac4
leonardobarilani <il_leonardo@hotmail.it>
2020-02-02 02:46:27 +0100

    working on repairs

---
+++ GGJ2020_ARRH/Assets/Scripts/Pirate/Drop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour
+{
+    public ScriptableLimb limb;
+
+    public float multiplier;
+
+}
---
+++ GGJ2020_ARRH/Assets/Scripts/UI/BackpackManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackpackManager : MonoBehaviour
+{
+    public static BackpackManager Instance;
+
+    void Start()
+    {
+        Instance = this;
+    }
+}
